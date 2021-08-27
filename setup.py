7a970681c4704764c5e9d01358b195fe89f77344
Yi Q <yitistica@outlook.com>
2021-08-27 19:08:15 +0800

    separate workflow

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-requirements = [ ]
+requirements = []
 
 test_requirements = ['pytest>=3', ]
 
@@ -37,6 +37,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/yitistica/cookiecutter_template_test',
-    version='0.1.0',
+    version='0.1.1',
     zip_safe=False,
 )
